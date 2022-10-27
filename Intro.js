7dc4daa03d0a9b302b343679cf17aee8e6320fdb
geekTrivedh <trivedhk@geekyants.com>
2022-10-27 11:26:38 +0530

    Initial commit

---
+++ Intro.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Profile from './components/Profile';
+
+class Intro extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>
+          This is a React Native snapshot test.
+        </Text>
+        <Profile />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+});
+
+export default Intro;
---
+++ components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const Profile = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 20}}>Profile</Text>
+    </View>
+  );
+};
+export default Profile;
